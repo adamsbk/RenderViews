054a8c34f96003a38548e02ac3193214e98f7e16
adamsbk <adamsabik@Adams-MacBook-Pro.local>
2015-02-13 01:01:31 +0100

    show directory tree of the model

--- GraphRenderer.js
+++ GraphRenderer.js
@@ -14,7 +14,19 @@
     self.initCalls.push(function () {
         window.console&&console.log('Just loaded');
         $(domQuery).text("Just loaded the graph renderer.");
+                        
+        var root = SeedWidgets.Instances()[0].GetShape(0);
+        self.WriteDirectoryTree(root, '');
+        
     });
+    
+    self.WriteDirectoryTree = function (node, spaces) {
+        $(domQuery).text(spaces + node.id);
+        var children = node.relations.children;
+        for (var i = 0; i < children.length; i++) {
+            self.WriteDirectoryTree(SeedWidgets.Instances()[0].GetShape(children[i]), spaces + '__');
+        }
+    }
     
     return self;
 }
