81481f49dd65280f2b59d6e13381a188300b69be
adamsbk <adamsabik@Adams-MacBook-Pro.local>
2015-04-11 12:40:43 +0200

    debug

--- GraphRenderer.js
+++ GraphRenderer.js
@@ -338,6 +338,7 @@ function ForceCollapsible(svg, width, height) {
         if (seedID === undefined) { //if seedID is undefined collapse each tree
             for (var seedID in self.trees) {
                 if (self.trees.hasOwnProperty(seedID)) {
+                    console.log("called t.hideNodes(level)");
                     self.trees[seedID].hideNodes(level);
                 }
             }
@@ -371,7 +372,9 @@ function ForceCollapsible(svg, width, height) {
     this.submitControls = function(event) {
         event.preventDefault();
         var level = $(this).find('#levelInput').val();
+        console.log(level);
         if (level >=0 && Math.floor(level) == level && $.isNumeric(level)) {
+            console.log("Collapse!");
             self.collapseTrees(level);
         }
     };
