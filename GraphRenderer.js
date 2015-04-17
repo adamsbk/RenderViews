76b751455fef719b31fb1ad7c63feb1dabb5bdb2
adamsbk <adamsabik@Adams-MacBook-Pro.local>
2015-04-17 13:45:50 +0200

    no message

--- GraphRenderer.js
+++ GraphRenderer.js
@@ -444,7 +444,8 @@ function ForceCollapsibleTree(tree, svg, focus) {
                     return d._children ? -Math.sqrt(d.descendatnCount) -8 : -10;
                 })
                 .chargeDistance(function(d) {
-                    return nodeRadius(d) * 5;
+                    console.log('r: ' + nodeRadius(d));
+                    return nodeRadius(d) * 30;
                 })
                 .linkDistance(function (d) {
                     var nodesRadius = nodeRadius(d.target) + nodeRadius(d.source);
